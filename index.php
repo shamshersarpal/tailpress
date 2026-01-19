3cffe027797694f955d0494a0e0e904bcbe4b4dd
shamshersarpal <shamshersingh01567@gmail.com>
2026-01-20 02:32:45 +0530

    PC Header

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Main template file for displaying posts.
  *
@@ -67,13 +68,13 @@ get_header();
 		<?php endif; ?>
 	<?php endif; ?>
 
-    <?php if (have_posts()): ?>
-        <?php while (have_posts()): the_post(); ?>
-            <?php get_template_part('template-parts/content', is_singular() ? 'single' : ''); ?>
-        <?php endwhile; ?>
+	<?php if (have_posts()): ?>
+		<?php while (have_posts()): the_post(); ?>
+			<?php get_template_part('template-parts/content', is_singular() ? 'single' : ''); ?>
+		<?php endwhile; ?>
 
-        <?php TailPress\Pagination::render(); ?>
-    <?php endif; ?>
+		<?php TailPress\Pagination::render(); ?>
+	<?php endif; ?>
 </div>
 
 <?php
---
+++ pages/home.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Template Name: Home Page
+ */
+
+get_header();
+?>
+
+    <section>
+        <div class="container mx-auto">
+            <h2 class="text-center text-red-800 text-4xl font-bold py-[200px]">Home Page Content Here!</h2>
+        </div>
+    </section>
+<?php
+get_footer();
